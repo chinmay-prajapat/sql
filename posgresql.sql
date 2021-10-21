0a7d8d50a7fe1fd299bc9126cac3c64b7c3500b8
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-21 13:00:12 +0530

    query added

--- posgresql.sql
+++ posgresql.sql
@@ -1,3 +1,4 @@
 --To count distinct actor from file
 SELECT COUNT(DISTINCT actor)from film;
+SELECT count(distinct title) FROM film WHERE rental_rate>4 AND replacement_cost >= 19.99 AND rating='R'
 
