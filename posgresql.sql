5b3bf22761e9dfb82a3a39343d0f2d770cb75c9a
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-28 14:18:11 +0530

    Update posgresql.sql
    update

--- posgresql.sql
+++ posgresql.sql
@@ -139,3 +139,7 @@ CREATE TABLE followers (
 );
 -- comments or photo_tags before '2010-01-07'
 select username,tags.created_at from users join (select user_id,created_at from caption_tags union all select user_id,created_at from photo_tags) as tags on tags.user_id=users.id where tags.created_at<'2010-01-07'
+with tags as
+	(select user_id,created_at from caption_tags union all select user_id,created_at from photo_tags)
+
+	select username,tags.created_at from users join tags on tags.user_id=users.id where tags.created_at<'2010-01-07'
