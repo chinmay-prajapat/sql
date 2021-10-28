42c93342b0dcde623bfdd934e4d796554c274cd2
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-28 14:03:11 +0530

    Update posgresql.sql
    update

--- posgresql.sql
+++ posgresql.sql
@@ -137,3 +137,5 @@ CREATE TABLE followers (
 	follower_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 	UNIQUE(leader_id, follower_id)
 );
+-- comments or photo_tags before '2010-01-07'
+select username,tags.created_at from users join (select user_id,created_at from caption_tags union all select user_id,created_at from photo_tags) as tags on tags.user_id=users.id where tags.created_at<'2010-01-07'
